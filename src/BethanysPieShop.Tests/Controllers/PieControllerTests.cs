3348efcd63cf85147d4149890b082ddffd68f483
bartekstacktrace <bartek@stacktrace.co.uk>
2022-12-13 19:19:14 +0000

    Adding new project to the solution - BethanysPieShop.Tests Adding unit tests for PieController List action method Adding repository mocks Adding unit tests for EmailTagHelper

---
+++ src/BethanysPieShop.Tests/Controllers/PieControllerTests.cs
@@ -0,0 +1,29 @@
+﻿using BethanysPieShop.Controllers;
+using BethanysPieShop.Tests.Mocks;
+using BethanysPieShop.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BethanysPieShop.Tests.Controllers
+{
+    public class PieControllerTests
+    {
+        [Fact]
+        public void List_EmptyCategory_ReturnsAllPies()
+        {
+            // Arrange
+            var mockPieRepository = RepositoryMocks.GetPieRepository();
+            var mockCategoryRepository = RepositoryMocks.GetCategoryRepository();
+
+            var pieController = new PieController(mockPieRepository.Object, mockCategoryRepository.Object);
+
+            // Act
+            var result = pieController.List("");
+
+            // Assert
+            var viewResult = Assert.IsType<ViewResult>(result);
+            var pieListViewModel = Assert.IsAssignableFrom<PieListViewModel>(viewResult.ViewData.Model);
+            Assert.Equal(10, pieListViewModel.Pies.Count());
+                
+        }
+    }
+}
